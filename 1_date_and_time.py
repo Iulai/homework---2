acf0d929eb6cdd76aad0d48856367beca8e0fc31
Yulay <yulay0506@gmail.com>
2021-12-01 17:21:35 +0500

    Добавил решения дз-2

---
+++ 1_date_and_time.py
@@ -0,0 +1,27 @@
+"""
+Домашнее задание №2
+
+Дата и время
+
+1. Напечатайте в консоль даты: вчера, сегодня, 30 дней назад
+2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
+
+"""
+from datetime import datetime, timedelta
+
+
+def print_days():
+    dt_now = datetime.now()
+    print(dt_now - timedelta(days=1))
+    print(dt_now)
+    print(dt_now - timedelta(days=30))
+
+
+def str_2_datetime(date_string):
+    date_dt = datetime.strptime(date_string.replace('.', ':'), '%d/%m/%y %H:%M:%S:%f')
+    return date_dt
+
+
+if __name__ == "__main__":
+    print_days()
+    print(str_2_datetime("01/01/20 12:10:03.234567"))
---
+++ 2_files.py
@@ -0,0 +1,25 @@
+"""
+Домашнее задание №2
+
+Работа с файлами
+
+
+1. Скачайте файл по ссылке https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0
+2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки
+3. Подсчитайте количество слов в тексте
+4. Замените точки в тексте на восклицательные знаки
+5. Сохраните результат в файл referat2.txt
+"""
+
+def main():
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+        print(len(content))
+        print(len(content.replace('\n', ' ').split(' ')))
+        content = content.replace('.', '!')
+        with open('referat2.txt', 'w', encoding='utf-8') as new_file:
+            text = new_file.write(content)
+
+
+if __name__ == "__main__":
+    main()
